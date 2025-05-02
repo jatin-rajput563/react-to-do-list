17cce7de9ee6ece504d193eed22b5bb87d9d43f4
jatin-rajput563 <jkt3298@gmail.com>
2025-05-02 11:11:15 +0530

    first commit

---
+++ src/components/List.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const List = () => {
+    return (
+        <>
+            <div className="min-h-screen justify-center flex items-center">
+                <div className="max-w-[1440px] mx-auto">
+                    <div>
+                        <input className='border-1 w-full max-w-[200px] py-2 px-3' type="text" placeholder='lewjfoire'/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default List
